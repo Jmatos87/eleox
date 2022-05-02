ab61090fc3c14ab4e78f575ee318d5f1c1c00c06
Josue Matos <josuematos87@gmail.com>
2022-05-02 13:53:58 -0500

    adjust modal for mobile

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -26,7 +26,7 @@ const modalContainer = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "100%",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
