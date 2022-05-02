b2cc9d9746c0985745c26b7e2818780aad8fd9aa
Josue Matos <josuematos87@gmail.com>
2022-05-02 14:09:02 -0500

    fix mobile modal bug

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -27,6 +27,7 @@ const modalContainer = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: "100%",
+  maxWidth: "400px",
   bgcolor: "background.paper",
   border: "2px solid #000",
   boxShadow: 24,
